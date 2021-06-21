7182b66ef1d09b53aa33f4b312a508447b4d526e
Randy Aubichon <randal.aub@sasktel.net>
2021-06-21 15:49:13 -0600

    first commit

---
+++ AlgorithmLibrary/Algorithms.cpp
@@ -0,0 +1,46 @@
+#include "Algorithms.h"
+#include <vector>
+#include <iostream>
+
+namespace Algorithms 
+{
+	/**
+	 * Run Time: 0(n)
+	 * Space Complexity: 0(1)
+	**/
+	void Searching::linearSearch(int value, std::vector<int> vector) {
+		for (unsigned int i = 0; i < vector.size(); i++) {
+			if (vector[i] == value) {
+				std::cout << "\nThe number is at index [" << i <<"].\n";
+				return;
+			}
+		}
+		std::cout << "\nThe number does not exist in the vertex.\n";
+		return;
+	}
+
+	/**
+	 * Run Time: 0(logn)
+	 * Space Complexity: 0(1)
+	**/
+	void Searching::binarySearch(int value, std::vector<int> vector) { //34
+		int start = 0; //
+		int end = vector.size(); //6
+
+		while (start <= end) {
+			int middle = (start + end ) / 2; //3
+			if (value == vector[middle]) {
+				std::cout << "\nThe number is at index [" << middle << "].\n";
+				return;
+			}
+			else if (value > vector[middle]) {
+				start = middle + 1;
+			}
+			else if (value < vector[middle]) {
+				end = middle - 1;
+			}
+		}
+		std::cout << "\nThe number does not exist in the vertex.\n";
+		return;
+	}
+}
---
+++ AlgorithmLibrary/Algorithms.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+
+namespace Algorithms
+{
+	class Searching 
+	{
+	public:
+		static void linearSearch(int value, std::vector<int> vector);
+		static void binarySearch(int value, std::vector<int> vector);
+	private:
+	};
+}
